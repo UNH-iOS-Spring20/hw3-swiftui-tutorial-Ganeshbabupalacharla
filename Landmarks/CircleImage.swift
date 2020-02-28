68176653d93cca6f84f83519b65a5850b02bc914
Palacharla <gpala1@LAB-BKH-226-W21.newhaven.local>
2020-02-27 23:58:25 -0500

    Tutorials

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  Circleimage.swift
+//  Landmarks
+//
+//  Created by Thota, Satish Kumar on 2/27/20.
+//  Copyright © 2020 Thota, Satish Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct Circleimage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Circleimage_Previews: PreviewProvider {
+    static var previews: some View {
+        Circleimage()
+    }
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Thota, Satish Kumar on 2/27/20.
+//  Copyright © 2020 Thota, Satish Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
